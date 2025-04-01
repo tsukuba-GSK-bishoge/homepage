5633801748154cadbb8907769726f67707722484
reversed_R <qiu1287shan@gmail.com>
2025-04-01 23:53:51 +0900

    fix: content layer

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,10 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const works = defineCollection({
-  loader: glob({ pattern: "*.md", base: "./pages/works/" }),
+  loader: glob({ pattern: "**/*.md", base: "src/pages/works/" }),
   schema: z.object({
     title: z.string(),
-    // slug: z.string(),
     description: z.string(),
-    // added: z.union([z.string(), z.date()]),
-    // updated: z.union([z.string(), z.date()]).optional(),
-    // tags: z.array(z.string()),
   }),
 });
 
