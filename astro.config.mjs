62fe120e4d40f510f7844bd69af62aa9af54a03b
USP113 <hikarisanshi11@gmail.com>
2025-04-17 12:16:00 +0900

    ヘッダー背景色と文字の強調色追 加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
+import { defineConfig } from 'astro/config';
+import googleFontsOptimizer from 'astro-google-fonts-optimizer';
+
+export default defineConfig({
+  integrations: [googleFontsOptimizer()],
+});
+
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+
+
+
