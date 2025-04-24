1281b26f677e12c22852ab194a3f3a41705ebce8
USP113 <hikarisanshi11@gmail.com>
2025-04-24 18:40:07 +0900

    CI修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 // import googleFontsOptimizer from 'astro-google-fonts-optimizer'; // ← 一時的に無効化
 
 export default defineConfig({
   // integrations: [googleFontsOptimizer()], // ← ここも無効化
 });
-
